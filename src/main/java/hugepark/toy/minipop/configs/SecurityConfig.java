2a7c0066c40e34ddd14baf10eb959ac0a95141da
hugepark1123 <huge.park.1123@gmail.com>
2017-08-22 02:10:48 +0900

    Security learning...

--- src/main/java/hugepark/toy/minipop/configs/SecurityConfig.java
+++ src/main/java/hugepark/toy/minipop/configs/SecurityConfig.java
@@ -9,64 +9,59 @@ package hugepark.toy.minipop.configs;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@EnableWebSecurity
+@EnableWebSecurity
 //@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
-	
-	@Order(Ordered.HIGHEST_PRECEDENCE)
-	@Configuration
-	protected static class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter {
-		@Override
-		public void init(AuthenticationManagerBuilder auth) throws Exception {
-			auth
-			.inMemoryAuthentication()
-			.withUser("admin")
-				.password("admin")
-				.roles("ADMIN", "USER", "ACTUATOR")
-			.and()
-			.withUser("user")
-				.password("user")
-				.roles("USER");
-		}
-	}
 	
 	@Configuration
 	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 	protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+		
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+		}
+		
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http
 			.authorizeRequests()
-			.anyRequest()
-				.fullyAuthenticated()
-			.and()
-				.formLogin()
-					.loginPage("/login")
-					.failureUrl("/login?error")
-				.permitAll()
-			.and()
-				.logout()
-				.permitAll();
+			.antMatchers("/hello/**").access("USER");
 		}
-
-		@Override
-		public void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth
-			.inMemoryAuthentication()
-				.withUser("admin")
-					.password("admin")
-					.roles("ADMIN", "USER")
-				.and()
-				.withUser("user")
-					.password("user")
-					.roles("USER");
-		}
+//
+//		@Override
+//		public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//			auth
+//			.inMemoryAuthentication()
+//				.withUser("admin")
+//					.password("admin")
+//					.roles("ADMIN", "USER")
+//				.and()
+//				.withUser("user")
+//					.password("user")
+//					.roles("USER");
+//		}
 	}
+	
+//	@Order(Ordered.HIGHEST_PRECEDENCE)
+//	@Configuration
+//	protected static class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter {
+//		@Override
+//		public void init(AuthenticationManagerBuilder auth) throws Exception {
+//			auth
+//			.inMemoryAuthentication()
+//			.withUser("admin")
+//				.password("admin")
+//				.roles("ADMIN", "USER", "ACTUATOR")
+//			.and()
+//			.withUser("user")
+//				.password("user")
+//				.roles("USER");
+//		}
+//	}
 }
